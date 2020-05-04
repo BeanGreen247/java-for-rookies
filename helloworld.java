16cb2210982ead774c466c01e1398d3a7d5f76b2
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-05-04 13:43:09 +0200

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+class MyClass {
+  public static void main(String[] args) {
+    String text = "This is my first app in Java";
+	System.out.println("Hello World!");
+	System.out.println(text);
+  }
+}
