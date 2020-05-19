a52e28a925edc6eeab88f4e03b9beb59873fc583
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-05-19 13:29:30 +0200

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,42 @@
+class MyClass {
+  public static void main(String[] args) {
+    byte num = 127;
+	System.out.println(num);
+
+	short num = 129;
+	System.out.println(num);
+
+	int num = 129;
+	System.out.println(num);
+
+	double num = 129.98;
+	System.out.println(num);
+
+	float num = 129.98f;
+	System.out.println(num);
+
+	boolean fun = true;
+	boolean learning = false;
+	System.out.println(fun);
+	System.out.println(learning);
+
+	char a = 65, b = 66, c = 67; 
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+
+	String a = "hi my name is Thomas"; 
+	System.out.println(a);
+
+	int intNum = 9;
+	double doubleNum = intNum;
+	
+	System.out.println(intNum);
+	System.out.println(doubleNum);
+
+	double doubleNum = 9.99;
+	int intNum = (int) doubleNum;
+	System.out.println(doubleNum);
+	System.out.println(intNum);
+  }
+}
